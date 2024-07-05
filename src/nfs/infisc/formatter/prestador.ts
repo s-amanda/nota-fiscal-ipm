f8a9690c4bcefb144ff375128a44baf9763a9db2
Amanda <amandadsp3@gmail.com>
2024-07-05 17:35:57 -0300

    corrige campo regime tributario infisc

--- src/nfs/infisc/formatter/prestador.ts
+++ src/nfs/infisc/formatter/prestador.ts
@@ -24,7 +24,7 @@ export function formatPrestador(empresa: Empresa) {
     },
     fone: empresa.fone,
     IE: '',
-    regimeTrib: 3,
+    regimeTrib: empresa.simplesNacional === 'S' ? 1 : 3,
   };
 
   return prest;
